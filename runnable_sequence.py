22eb64251c1172ec9717593d2bacb49571b34643
Goldi Rathore <goldirathore2003@gmail.com>
2025-11-27 23:43:28 +0530

    Added new files / updated files

---
+++ runnable_sequence.py
@@ -0,0 +1,37 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import (
+    RunnableSequence,
+   
+)
+
+
+load_dotenv()
+
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke : \n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+result = chain.invoke({'topic':'AI'})
+
+print(result)
